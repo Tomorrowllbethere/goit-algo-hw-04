02e771b0cb4caede889c4c1f73ce3bd4a2d032d9
Tomorrowllbethere <uliadiakiv@gmail.com>
2024-02-03 12:41:23 +0100

    фінальна версія коду Обчислення суми зарплат і середньої зарплати, виятгнувши дані з файла

---
+++ main.py
@@ -0,0 +1,18 @@
+import pathlib
+def total_salary(path):
+     with open(path, "r") as salary_data:   # функція  приймає текстовий файл
+        # перебираємо кожен рядок, створюємо список рядків
+        lines = [el.strip() for el in salary_data.readlines()]
+        num=0 #лічильник ітерацій
+        total=0 #змінна, що отримає суму чисел
+        for i in lines: # перебираємо рядок в списку рядків
+            s=i.split(',') # розділяємо через кому рядок
+            a=int(s[1]) #отримуємо число через індекс
+            total+=(a) #додаємо число в суму
+            num+=1 #додаємо ітерацію в лічильник
+        average=int(total/num) #функція рахує середню зарплату і приводить її в ціле число   
+        return (total, average) #повертаємо значення
+current_dir = pathlib.Path(__file__).parent #отримуємо шлях поточного файлу
+path=(current_dir/"salary.txt") #отримуємо шлях до текстового файла з даними про зп
+total, average = total_salary(path) #витягуємо змінні з функції
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}") #виводимо значення
